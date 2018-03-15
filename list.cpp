5242637b68033a5253939ac7a715786321446781
trahatar <36998161+trahatar@users.noreply.github.com>
2018-03-15 17:33:07 +0100

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -7,35 +7,63 @@ struct node
 	node* next=NULL;
 	node* prev=NULL;
 };
-void add(node*& head, int value)
+void show(node* head)
 {
-	node* Node=new node;
-	Node->value = value;
-	Node->next = head;
-	if (Node->next != NULL)
+	while (head != NULL)
 	{
-		Node->next->prev = Node;
+		cout << head->value << '\t';
+		head = head->next;
 	}
-	head = Node;
-	//cout << "In add \n value \t next \t\t prev \t\t adress \n" << Node->value << '\t' << Node->next << '\t' << Node->prev <<'\t'<< Node<<endl;
-
+	cout<<endl;
 }
-void add(node*& head, int value, int position)//should use add(node*& head, int value)
+void add(node*& head, int value, int position)//adds element at position. If position<=0 element goes at the start; if position>=number of elements, it goes at the end
 {
 	node* temp=head;
-	for (int i = 0; i < position; i++)
+	if (position<=0||head==0)
+    {
+        temp=new node;
+        temp->value=value;
+        temp->next=head;
+        if (head) head->prev=temp;
+        head=temp;
+        return;
+    }
+    int i=1;
+    node* Node = new node;
+    Node->value = value;
+	while(1)
 	{
-		if (temp->next!=NULL) temp=temp->next;
+		if (temp->next!=NULL&&i<position) temp=temp->next;
+		else if (temp->next==NULL)
+        {
+            if (temp->next==0)//adding at the last place in the list
+            {
+                Node->next=0;
+                Node->prev=temp;
+                temp->next=Node;
+                return;
+            }
+            Node->next = temp;
+            Node->prev=temp->prev;
+            temp->prev=Node;
+            if (Node->prev) Node->prev->next=Node;
+            return;
+		}
+		else
+        {
+            Node->next=temp->next;
+            Node->prev=temp;
+            temp->next=Node;
+            Node->next->prev=Node;
+            return;
+        }
+		i++;
 	}
-	node* Node = new node;
-	Node->value = value;
-	//cout<<head->next<<endl;
-	Node->next = temp;
-	Node->prev=temp->prev;
-	if (Node->next != NULL)
-	{
-		Node->next->prev = Node;
-	}
+
+}
+void add(node*& head, int value)
+{
+	add(head,value,0);
 }
 void readFromFile(node*& head, char* filename)
 {
@@ -46,10 +74,9 @@ void readFromFile(node*& head, char* filename)
 	{
 		while (!F.eof())
 		{
-		    a=NULL;
+		    a=0;
 			F >> a;
-			if (F.tellg()!=-1)add(head, a);
-			//cout << a << endl;
+			if (F.tellg()!=-1)add(head, a,100);
 		}
 	}
 }
@@ -61,28 +88,43 @@ void remove(node*& head, int a)
 		//cout << "in remove \n value \t next \t\t prev \t\t adress \n" << temp->value << '\t' << temp->next << '\t' << temp->prev << '\t' << temp << endl;
 		if (temp->value == a)
 		{
-			if (temp->prev!=NULL) temp->prev->next = temp->next;
+			if (temp->prev!=NULL)
+            {
+                temp->prev->next = temp->next;
+
+            }
 			else head = temp->next;
 		}
 		temp = temp->next;
 	}
 }
-void show(node* head)
-{
-	while (head != NULL)
-	{
-		cout << head->value << endl;
-		head = head->next;
-	}
-}
 int main()
 {
 	node *head=NULL;
-	//readFromFile(head, "liczby.txt");
-	add(head,5);
-	for (int i=0; i<20; i++) add(head,i,i);
-	show(head);
+    show(head);
+    int i=100;
+	while (1)
+    {
+        //cout<<"1:add at given pos; 2:delete at given pos; 3:read from file"<<endl;
+        int n=1;
+        //cin>>n;
+        cout<<endl;
+        switch(n)
+        {
+            case 1:
+                cin>>n;
+                cout<<endl;
+                add(head,i,n);
+                show(head);
+                i++;
+                break;
+            case 2:
+                cin>>n;
+                cout<<endl;
+                remove(head, 5);
+            case 3:
+                readFromFile(head, "liczby.txt");
+        }
+    }
 	cout << endl;
-	//remove(head, 5);
-	show(head);
 }
